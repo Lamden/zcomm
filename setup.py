b8799507fa209df4d2354dd21db1242bd487c836
Stuart Farmer <stuart@lamden.io>
2019-10-11 16:15:53 -0700

    first

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='zcomm',
+    version='0.0.1',
+    packages=['zcomm'],
+    url='https://github.com/Lamden/zcomm',
+    license='GPL3',
+    author='Stuart Farmer',
+    author_email='stuart@lamden.io',
+    description='PyZMQ structures and objects to help write microservices.'
+)
